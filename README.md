48ab07a91d25c5ab8e0c63e94a694480960d06fd
Redy1908 <58226371+Redy1908@users.noreply.github.com>
2025-06-16 17:38:48 +0200

    Updated

--- README.md
+++ README.md
@@ -15,7 +15,6 @@
    ```bash
    echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc && source ~/.bashrc
    echo "source /usr/share/gazebo/setup.sh" >> ~/.bashrc && source ~/.bashrc
-   echo "export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/<PATH-TO-NAVIGO>/ros_ws/src/litterbug/models" && source ~/.bashrc
    echo 'export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp' >> ~/.bashrc && source ~/.bashrc
    ```
 
@@ -53,3 +52,20 @@ If you want to create a new map:
 cd ros_ws
 ros2 launch aws_robomaker_small_house_world house_mapping.launch.py 
 ```
+
+# Navigation test
+
+(Assuming you have a map ready or using the provided one)
+
+```bash
+cd ros_ws
+ros2 launch aws_robomaker_small_house_world house_map.launch.py 
+```
+
+Then use the `2D Pose Estimate` tool to set the robot's initial position and orientation on the map.
+(Align thre green arerow to the robot's red arrow)
+
+Then you can use the `2D Nav Goal` tool to set a goal position for the robot.
+
+The robot should navigate to the goal position using the map and the navigation stack.
+
--- ros_ws/src/aws-robomaker-small-house-world-ros2/launch/house_map.launch.py
+++ ros_ws/src/aws-robomaker-small-house-world-ros2/launch/house_map.launch.py
@@ -8,32 +8,43 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
+
 os.environ["GAZEBO_MODEL_PATH"] = path.join(
     get_package_share_directory("litterbug"), "models"
 )
 
+
 def generate_launch_description():
-    config_dir = os.path.join(get_package_share_directory("explorer"), "config")
-    rviz_config = os.path.join(config_dir, "nav.rviz")
-    
     launch_file_dir = os.path.join(
         get_package_share_directory("turtlebot3_gazebo"), "launch"
     )
 
     pkg_gazebo_ros = get_package_share_directory("gazebo_ros")
-    pkg_nav2_bringup = get_package_share_directory("nav2_bringup")
-    pkg_slam_toolbox = get_package_share_directory("slam_toolbox")
-    pkg_nav2_map_server = get_package_share_directory("nav2_map_server")
 
     house_world_path = os.path.join(
         get_package_share_directory("aws_robomaker_small_house_world"),
         "worlds",
         "house.world",
     )
+    map_file = os.path.join(
+        get_package_share_directory("aws_robomaker_small_house_world"),
+        "maps",
+        "house.yaml",
+    )
+
+    params_file = os.path.join(
+        get_package_share_directory("aws_robomaker_small_house_world"),
+        "param",
+        "tb3_nav_params.yaml",
+    )
+
+    config_dir = os.path.join(get_package_share_directory("explorer"), "config")
+    rviz_config = os.path.join(config_dir, "nav.rviz")
 
     use_sim_time = LaunchConfiguration("use_sim_time", default="true")
     x_pose = LaunchConfiguration("x_pose", default="0.0")
     y_pose = LaunchConfiguration("y_pose", default="0.0")
+    yaw_pose = LaunchConfiguration("yaw_pose", default="0.0")
 
     gzserver_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -59,29 +70,21 @@ def generate_launch_description():
         PythonLaunchDescriptionSource(
             os.path.join(launch_file_dir, "spawn_turtlebot3.launch.py")
         ),
-        launch_arguments={"x_pose": x_pose, "y_pose": y_pose}.items(),
+        launch_arguments={"x_pose": x_pose, "y_pose": y_pose, "yaw": yaw_pose}.items(),
     )
 
-    nav2_cmd = IncludeLaunchDescription(
+    nav2 = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(pkg_nav2_bringup, "launch", "navigation_launch.py")
+            [
+                get_package_share_directory("nav2_bringup"),
+                "/launch",
+                "/bringup_launch.py",
+            ]
         ),
-        launch_arguments={
-            "use_sim_time": "true",
-        }.items(),
+        launch_arguments={"map": map_file, "params_file": params_file}.items(),
     )
 
-    slam_cmd = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(pkg_slam_toolbox, "launch", "online_async_launch.py")
-        ),
-        launch_arguments={
-            "use_sim_time": "true",
-            "publish_period": "0.0",
-        }.items(),
-    )
-
-    rviz2 = Node(
+    rviz = Node(
         package="rviz2",
         output="screen",
         executable="rviz2",
@@ -89,33 +92,13 @@ def generate_launch_description():
         arguments=["-d", rviz_config],
     )
 
-    explorer = Node(
-        package="explorer",
-        executable="explorer",
-        name="explorer_node",
-        output="screen",
-        parameters=[],
-    )
-
-    map_saver_server = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(pkg_nav2_map_server, "launch", "map_saver_server.launch.py")
-        ),
-        launch_arguments={
-            "use_sim_time": "true",
-        }.items(),
-    )
-
-    sim_group = GroupAction(actions=[gzserver_cmd, gzclient_cmd, robot_state_publisher_cmd, spawn_turtlebot_cmd])
-    nav_group = GroupAction(actions=[nav2_cmd, slam_cmd, map_saver_server])
-    ui_group = GroupAction(actions=[rviz2])
-    explorer_group = GroupAction(actions=[explorer])
-
     ld = LaunchDescription()
 
-    ld.add_action(sim_group)
-    ld.add_action(nav_group)
-    ld.add_action(ui_group)
-    ld.add_action(explorer_group)
+    ld.add_action(gzserver_cmd)
+    ld.add_action(gzclient_cmd)
+    ld.add_action(robot_state_publisher_cmd)
+    ld.add_action(spawn_turtlebot_cmd)
+    ld.add_action(nav2)
+    ld.add_action(rviz)
 
     return ld
